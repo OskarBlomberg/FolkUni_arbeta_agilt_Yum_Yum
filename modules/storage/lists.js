7e49f9e99f7dce4cdfe85c9f95b38b297d2f5f4e
Oskar Blomberg <blomberg.oskar@gmail.com>
2025-03-12 13:34:46 +0100

    global order object with lists

---
+++ modules/storage/lists.js
@@ -0,0 +1,5 @@
+const orders = {
+  current: {}, // Kundens nuvarande order
+  previous: [], // Tidigare ordrar (måste ha date.now()-ordernummer som id. Sista i arrayen är senaste)
+  toRestaurant: [], // Restaurangens orderlista
+};
