8ba39179a7ec84ad9589927798debc28236f995a
adamekholt <adam.ekholt@hotmail.com>
2025-03-13 08:08:54 +0100

    har lagt handlekurviconet til i navbaren, funksjonaliteten legges til når mer kode er oppdatert

---
+++ modules/storage/localStorage.js
@@ -0,0 +1,15 @@
+export function arrFromStorage(key) {
+    return JSON.parse(localStorage.getItem(key)) || [];
+  }
+  
+  export function arrToStorage(arr, key) {
+    localStorage.setItem(key, JSON.stringify(arr));
+  }
+  
+  export function objToStorage(obj, key) {
+    localStorage.setItem(key, JSON.stringify(obj));
+  }
+  
+  export function objFromStorage(key) {
+    return JSON.parse(localStorage.getItem(key)) || {};
+  }
